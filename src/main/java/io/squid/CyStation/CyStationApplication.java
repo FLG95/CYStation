f3f0602c48ea8e5980f96a16f86681593cda4d4c
FG95 <florian.goupil@icloud.com>
2026-03-31 17:52:00 +0200

    seting up framework

--- src/main/java/io/squid/CyStation/CyStationApplication.java
+++ src/main/java/io/squid/CyStation/CyStationApplication.java
@@ -1,13 +1,20 @@
 package io.squid.CyStation;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CyStationApplication {
+public class CyStationApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CyStationApplication.class, args);
-	}
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
 
 }
+
--- src/test/java/io/squid/CyStation/CyStationApplicationTests.java
+++ src/test/java/io/squid/CyStation/CyStationApplicationTests.java
@@ -3,11 +3,13 @@ package io.squid.CyStation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class CyStationApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+    
 
 }
