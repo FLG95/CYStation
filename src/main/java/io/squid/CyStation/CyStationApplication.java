80dbc9cc44421a470b5f081299004bae9eb49466
FG95 <florian.goupil@icloud.com>
2026-03-31 16:12:35 +0200

    add start

---
+++ src/main/java/io/squid/CyStation/CyStationApplication.java
@@ -0,0 +1,13 @@
+package io.squid.CyStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyStationApplication.class, args);
+	}
+
+}
