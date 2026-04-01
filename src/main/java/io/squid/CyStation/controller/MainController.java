37f95807fadb41e2cbecde7786435403a877eb60
FG95 <florian.goupil@icloud.com>
2026-04-01 15:54:38 +0200

    configure footer and header inclusion

--- src/main/java/io/squid/CyStation/controller/MainController.java
+++ src/main/java/io/squid/CyStation/controller/MainController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+
     @RequestMapping("/")
     public String index(){
         return "public/index";
     }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "public/about";
+    }
+
 }
