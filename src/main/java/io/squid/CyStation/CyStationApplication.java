75c27d2eb06a3fa49178a9e4f6da7d002945b93d
FLG95 <florian.goupil@icloud.com>
2026-04-24 17:47:08 +0200

    clean

--- src/main/java/io/squid/CyStation/CyStationApplication.java
+++ src/main/java/io/squid/CyStation/CyStationApplication.java
@@ -1,13 +1,9 @@
 package io.squid.CyStation;
 
-import io.squid.CyStation.model.Co2Sensor;
-import io.squid.CyStation.model.User;
-import io.squid.CyStation.repository.DeviceRepository;
-import io.squid.CyStation.repository.UserRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -18,28 +14,12 @@ public class CyStationApplication implements CommandLineRunner {
     }
 
 
-    /*
-    // Pr s'exec à chaque démarrage du serv (testing)
-    @Bean
-    public CommandLineRunner testCreation(DeviceRepository deviceRepository) {
-        return args -> {
-            Co2Sensor capteur = new Co2Sensor();
-            capteur.setName("Détecteur Sas Principal");
-            capteur.setPpmLevel(450);
-
-            deviceRepository.save(capteur);
-
-            System.out.println("Capteur sauvegardé dans MariaDB.");
-        };
-    }*/
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Server started");
 
     }
-
-
 
 }
 
