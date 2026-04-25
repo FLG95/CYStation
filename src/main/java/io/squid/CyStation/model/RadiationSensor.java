088a602a7ebd3207e5a42875ce22961ea4e49219
HichamEjh <166216207+HichamEjh@users.noreply.github.com>
2026-04-25 11:39:19 +0300

    a

--- src/main/java/io/squid/CyStation/model/RadiationSensor.java
+++ src/main/java/io/squid/CyStation/model/RadiationSensor.java
@@ -1,16 +1,1 @@
-package io.squid.CyStation.model;
 
-import io.squid.CyStation.enums.DeviceCategory;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-
-@Entity
-@DiscriminatorValue("RADIATION_SENSOR")
-public class RadiationSensor extends Sensor{
-
-
-    @Override
-    public String getDeviceDisplayName() {
-        return DeviceCategory.RADIATION_SENSOR.getDisplayName();
-    }
-}
