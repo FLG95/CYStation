fc32ce1a458be4d1944c86194a51e41eb0bcaffc
FLG95 <florian.goupil@icloud.com>
2026-04-27 19:05:22 +0200

    add whitelist system

--- src/main/java/io/squid/CyStation/config/MyUserDetailsService.java
+++ src/main/java/io/squid/CyStation/config/MyUserDetailsService.java
@@ -3,13 +3,12 @@ package io.squid.CyStation.config;
 import io.squid.CyStation.model.User;
 import io.squid.CyStation.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
@@ -23,13 +22,15 @@ public class MyUserDetailsService implements UserDetailsService {
         User user = userRepository.findUserByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé avec l'email : " + email));
 
-
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
                 .authorities(user.getRole().name())
+                .disabled(!user.isEnable())
                 .build();
     }
+
+
 
 
 }
