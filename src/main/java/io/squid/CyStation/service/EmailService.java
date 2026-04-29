1898eacf9b6af113d8e2e0fcf64dab303f067cda
HichamEjh <166216207+HichamEjh@users.noreply.github.com>
2026-04-29 18:00:38 +0300

    mail

--- src/main/java/io/squid/CyStation/service/EmailService.java
+++ src/main/java/io/squid/CyStation/service/EmailService.java
@@ -14,8 +14,16 @@ public class EmailService {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("ton-email@gmail.com");
         message.setTo(toEmail);
-        message.setSubject("Bienvenue parmi nous !");
-        message.setText("Bonjour " + username + ",\n\nMerci de vous être inscrit sur notre plateforme.");
+        message.setSubject("Confirmation de votre inscription à CYStation");
+        message.setText("Bonjour " + username + ",\n\nNous sommes ravis de vous ompter parmi nous. Ce message vous confirme que votre inscription à CYStation a bien été validée.\n\n" +
+                "La sécurité de la station repose sur une hiérarchie stricte. Vous ne pouvez pas tout contrôler dès le début. Plus vous interagissez avec les systèmes sécurisés, plus vous gagnez de points qui débloquent de nouvelles responsabilités.\n\n" +
+                "\uD835\uDDE0\uD835\uDDF2\uD835\uDDFA\uD835\uDDEF\uD835\uDDFF\uD835\uDDF2 \uD835\uDDF1'\uD835\uDDF2́\uD835\uDDFE\uD835\uDE02\uD835\uDDF6\uD835\uDDFD\uD835\uDDEE\uD835\uDDF4\uD835\uDDF2 : Accès aux équipements sans dangers pour la station.\n" +
+                "\uD835\uDDE6\uD835\uDDF0\uD835\uDDF6\uD835\uDDF2\uD835\uDDFB\uD835\uDE01\uD835\uDDF6\uD835\uDDF3\uD835\uDDF6\uD835\uDDFE\uD835\uDE02\uD835\uDDF2 : Accès aux outils de surveillance et des ressources vitales. Vous pouvez demander la suppression d'appareils.\n" +
+                "\uD835\uDDD4\uD835\uDDF1\uD835\uDDFA\uD835\uDDF6\uD835\uDDFB\uD835\uDDF6\uD835\uDE00\uD835\uDE01\uD835\uDDFF\uD835\uDDEE\uD835\uDE01\uD835\uDDF2\uD835\uDE02\uD835\uDDFF : Contrôle total. Supervision des paramètres critiques et accès au panel exclusif pour gérer l'équipqge.\n\n" +
+                "Spatialement,\n" +
+                "CYStation ");
+        
+
 
         mailSender.send(message);
         System.out.println("Email envoyé avec succès à " + toEmail);
